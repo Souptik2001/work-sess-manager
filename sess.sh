99db73623adb838e995212c75117d57f660fa1c2
Souptik Datta <souptikdatta2001@gmail.com>
2023-11-18 14:22:15 +0530

    feat: Add drop-ins functionality
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- sess.sh
+++ sess.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+drop_ins () {
+	copy_me $1
+}
+
+# Copy the `copy_me()` drop-in and create your own drop-ins, and call them in the `drop_ins()` function.
+copy_me () {
+	echo "Doing $1!"
+}
+
 if [ -z "$1" ]  || [ -z "$2" ]; then
 	echo 'Usage: sess <any_string> <any_integer_duration>'
 else
 	val=$1
 	duration=$2
+	drop_ins $1
 	echo $val | lolcat
 	timer ${duration}m -n ${val}
 	notify-send -u critical -i face-yawn "👋" "'$duration' minutes '$val' session done."
