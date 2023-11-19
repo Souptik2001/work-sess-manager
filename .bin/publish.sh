a9f72986193d13b84b1b1040cc0d24454afe68b7
Souptik Datta <souptikdatta2001@gmail.com>
2023-11-19 15:22:43 +0530

    chore: Update workflow
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- .bin/publish.sh
+++ .bin/publish.sh
@@ -10,11 +10,11 @@ mkdir -p ~/.ssh && echo "StrictHostKeyChecking no" >> ~/.ssh/config
 
 # Git Config
 git config --global user.email "bot@souptik.dev"
-git config --global user.name "GitHub Deploy"
+git config --global user.name "GitHub Publish"
 
 # Clone AUR Repo
-git clone --single-branch --branch $AUR_GIT_BRANCH $AUR_GIT_REPOSITORY $HOME/aur-deploy --depth 1
-cd $HOME/aur-deploy
+git clone --single-branch --branch $AUR_GIT_BRANCH $AUR_GIT_REPOSITORY $HOME/aur-publish --depth 1
+cd $HOME/aur-publish
 
 # Copy PKGBUILD.
 rm -rf PKGBUILD
@@ -27,7 +27,7 @@ cp -r $GITHUB_WORKSPACE/.SRCINFO ./.SRCINFO
 # Push the changes!
 git add .
 git status
-git commit -m "Deploy from GitHub $GITHUB_SHA"
+git commit -m "Publish from GitHub $GITHUB_SHA"
 git push origin $AUR_GIT_BRANCH
 
 set +x
